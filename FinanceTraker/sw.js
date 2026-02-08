b6a27e9d89d0429fe274f033d96e61d790d7ce07
Răzvan <82700436+rzvn332@users.noreply.github.com>
2026-02-08 02:22:25 +0200

    Add service worker for caching assets

---
+++ FinanceTraker/sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'finance-v1';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
